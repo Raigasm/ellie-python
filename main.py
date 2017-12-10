242420cc8790d22b7bbdaa50e789a17833f869d0
Rai <rai@rbutera.com>
2017-12-10 17:52:42 +0000

    almost at drawing something

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
 # import the Tkinkter module
 import tkinter as tkinter
+import circle
+
 # create a new window
 
-window = tkinter.Tk()
+
+def MakeView():
+    window = tkinter.Tk()
+    window.title("Visual Experiment")
+    window.geometry("1024x768")
+    window.mainloop()
+    canvas = tkinter.Canvas(window, width=1024, height=768,
+                            borderwidth=1, highlightthickness=1, bg="black")
+    canvas.grid()
+    canvas.create_circle(100, 120, 50, fill="white",
+                         outline="ddd", width=4)
+    return window
+
 
 # draw the window and start the application
-window.mainloop()
+
+
+def main():
+    """main function for all program logic"""
+    view = MakeView()
+    # access to view
+
+
+main()
